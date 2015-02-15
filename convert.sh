938176d41cdd5a1a31e3f140510052ee5d742845
Afa <afa@afa.moe>
2015-02-14 19:15:46 -0500

    On branch master 	renamed:    mgConvert.sh -> convert.sh 	modified:   mgConvert.sh

---
+++ convert.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+src=$1
+des=$2
+width=$3
+height=$4
+watermark=$5
+
+##### Queue
+# If convert is already running, i.e. file exsits, retry 15 times with 1 sec interval
+lockfile -1 -r15 conv.lock
+locked=$?
+# Doesn't matter if it is locked
+# Will commit to convert anyway
+function CleanExit() {
+    # If successfully locked, rm the file
+    if [ $locked -eq 0 ]
+    then
+        rm -f conv.lock
+    fi
+}
+
+
+if [ `identify -format "%n" ${src}` -eq 1 ]
+then
+	convert ${src} -resize ${width}x${height} ${des}
+elif [ `identify -format "%n" ${src}` -le 10 ]
+then
+	convert ${src} -coalesce gif:- | convert gif:- -resize ${width}x${height} ${des}
+else
+	convert ${src}[0] -resize ${width}x${height} ${des}
+	convert -background green -size 30x20 -gravity center -fill white -font helvetica -pointsize 12 label:GIF gif:- | composite -gravity NorthEast gif:- ${des} ${des}
+fi
+
+if [ ${width} -le 300 ]
+	then CleanExit
+fi
+
+
+if [ `identify -format "%n" ${src}` -eq 1 ]
+then
+	composite -gravity SouthEast -dissolve 50 ${watermark} ${des} ${des}
+elif [ `identify -format "%n" ${src}` -le 10 ]
+then
+	convert ${des} -gravity SouthEast -geometry +0+0  null: ${watermark} -compose dissolve -define compose:args=50 -layers composite -layers optimize ${des};
+else
+	composite -gravity SouthEast -dissolve 50 ${watermark} ${des} ${des}
+fi
+CleanExit
+
