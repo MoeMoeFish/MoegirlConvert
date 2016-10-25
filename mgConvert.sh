cf74067f38804098755ebcd770a33e3833e8bde8
Mingye Wang <arthur200126@gmail.com>
2016-10-25 17:43:05 -0400

    forward all args

--- mgConvert.sh
+++ mgConvert.sh
@@ -2,4 +2,4 @@
 
 # Don't know if it is sync or async in MW call
 # Will make it async anyway
-nohup $PWD/convert.sh $1 $2 $3 $4 $5  >/dev/null 2>&1 &
+nohup "$PWD/convert.sh" "$@" >/dev/null 2>&1 &
